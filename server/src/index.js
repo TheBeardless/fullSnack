0eb8caf97b3d1c2530310e18bf5aca0a5a3b7d01
Jake Webster <jakepeterwebster@gmail.com>
2021-01-30 10:28:44 +1000

    encountered unknown errors setting up routes, jumping to models

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const session = require("express-session"); // though I can't remember why.
+const session = require("express-session"); // for cookies
 
 // import all routers here.
+// const internalRouter = require("./routes/internalRoutes");
 
-// connect mongoDB/mongoose here.
+// // connect mongoDB/mongoose here.
+mongoose.connect("mongodb://localhost:27017/fullSnack", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 // initialise the app
 const app = express();
@@ -16,12 +21,13 @@ const port = 3000;
 // app (express) middleware usage
 app.use(express.json());
 
-// paste something to the backend page.
+// paste something to the backend / index route page.
 app.get("/", (request, response) =>
-  response.send("This is the backend server")
+  response.send("This is the index.js backend server")
 );
 
 // tell express that it needs to use the routers we have intialised
+// app.use("/internal", internalRouter);
 
 // quality of life, confirm port response.
 app.listen(port, () =>
---
+++ server/src/routes/internalRoutes.js
@@ -0,0 +1,13 @@
+// // import express
+// const express = require("express");
+
+// // this will handle all internal routes
+// const router = express.Router();
+
+// // routes
+// router.get("/", (request, response) =>
+//   response.send("This is the backend server")
+// );
+
+// // Export the router so it can be used by index.js
+// module.exports = router;
